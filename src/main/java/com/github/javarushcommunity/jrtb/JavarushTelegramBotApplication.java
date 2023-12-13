621a416f043ec5b06a7729c311a67fd1485b11f1
Victor <131241055+panzerKotwagen@users.noreply.github.com>
2023-12-13 20:07:49 +0300

    JRTB-2: Added stub Telegram bot to the project. (#5)
    * JRTB-2: add stub Telegram bot
    
    * JRTB-2: update project version and added to RELEASE_NOTES

--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -2,8 +2,13 @@ package com.github.javarushcommunity.jrtb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"package com.github.javarushcommunity.jrtb",
+		"org.telegram.telegrambots"
+})
 public class JavarushTelegramBotApplication {
 
 	public static void main(String[] args) {
